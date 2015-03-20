7afced5ff2745d09f95fb3f9a773b8fc025fd8b0
pietrodegrazia <pietrodegrazia@hotmail.com>
2015-03-20 17:04:36 -0300

    commit

---
+++ Pivy/CollectionViewController.h
@@ -0,0 +1,15 @@
+//
+//  CollectionViewController.h
+//  Pivy
+//
+//  Created by Pietro Degrazia on 3/20/15.
+//  Copyright (c) 2015 Henrique Valcanaia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewController : UICollectionViewController <UICollectionViewDataSource, UICollectionViewDelegate>
+
+
+@end
