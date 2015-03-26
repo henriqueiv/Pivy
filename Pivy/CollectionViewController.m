150bc38a281fdccb83e1676bf38605acf23dab00
pietrodegrazia <pietrodegrazia@hotmail.com>
2015-03-26 14:50:09 -0300

    collection view com as "ivys" funcionando.

--- Pivy/CollectionViewController.m
+++ Pivy/CollectionViewController.m
@@ -14,12 +14,14 @@
 #import "AppDelegate.h"
 
 @interface CollectionViewController()
- @property NSString *reuseIdentifier;
- @property NSMutableDictionary *pivyDic;
- @property NSMutableArray *countries;
- @property NSSet *countrySet;
- @property NSMutableArray *pivyArray;
+
+@property NSString *reuseIdentifier;
+@property NSMutableDictionary *pivyDic;
+@property NSMutableArray *countries;
+@property NSMutableArray *pivyArray;
+
 @end
+
 
 @implementation CollectionViewController
 
@@ -36,39 +38,29 @@
     PFQuery *query = [Pivy query];
     [query fromLocalDatastore];
     [query orderByAscending:@"Country"];
+    
     NSArray *objects = [[NSArray alloc]init];
     objects = [query findObjects];
     
-    for(Pivy *pivy in objects){
-//        NSLog(@"%@", pivy.Country);
-    }
-    
-    
-    NSString *currentCountry = [[NSString alloc]init];
-    currentCountry = [[objects firstObject] Country];
-    
     self.pivyDic = [[NSMutableDictionary alloc]init];
-//    self.pivyArray = [[NSMutableArray alloc]init];
-//    [self.pivyDic setObject:self.pivyArray forKey:@"China"];
+    
+    self.countries = [[NSMutableArray alloc]init];
+    
+    
     for (Pivy *pivy in objects) {
+        
         if ([self.pivyDic objectForKey:pivy.Country]) {
+            
             [[self.pivyDic objectForKey:pivy.Country] addObject:pivy];
-//            NSLog(@"--- KEY: %@ --- OBJ: %@ --",pivy.Country, pivy.name);
         }
         else{
             NSMutableArray *pivyArray = [[NSMutableArray alloc]initWithObjects:pivy, nil];
+            
             [self.pivyDic setObject:pivyArray forKey:pivy.Country];
             
-//            NSLog(@"--- ELSE ---");
+            [self.countries addObject:pivy.Country];
         }
     }
-//    NSLog(@"DICIONARIO");
-//    NSLog(@"%@", self.pivyDic);
-//    
-//    NSLog(@"CONTAGENS");
-//    NSLog(@"%ld", self.pivyDic.count);
-   
-    
     
     UICollectionViewFlowLayout *layout = (UICollectionViewFlowLayout*)self.collectionView.collectionViewLayout;
     layout.sectionInset = UIEdgeInsetsMake(15, 0, 15, 0);
@@ -79,23 +71,23 @@
 //- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
 //{
 //    UICollectionReusableView *reusableview = nil;
-//    
+//
 //    if (kind == UICollectionElementKindSectionHeader) {
 //        RecipeCollectionHeaderView *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"HeaderView" forIndexPath:indexPath];
 //        NSString *title = [[NSString alloc]initWithFormat:@"Recipe Group #%i", indexPath.section + 1];
 //        headerView.title.text = title;
 //        UIImage *headerImage = [UIImage imageNamed:@"header_banner.png"];
 //        headerView.backgroundImage.image = headerImage;
-//        
+//
 //        reusableview = headerView;
 //    }
-//    
+//
 //    if (kind == UICollectionElementKindSectionFooter) {
 //        UICollectionReusableView *footerview = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier:@"FooterView" forIndexPath:indexPath];
-//        
+//
 //        reusableview = footerview;
 //    }
-//    
+//
 //    return reusableview;
 //}
 
@@ -103,35 +95,45 @@
 - (void)collectionView:(UICollectionView *)collectionView
 didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
     
-//    self.collectionView.backgroundColor = [UIColor blueColor];
-//    self.collectionView.backgroundView.backgroundColor = [UIColor brownColor];
+    //    self.collectionView.backgroundColor = [UIColor blueColor];
+    //    self.collectionView.backgroundView.backgroundColor = [UIColor brownColor];
     UICollectionViewCell *cell = [self.collectionView cellForItemAtIndexPath:indexPath];
     cell.backgroundColor = [UIColor blackColor];
     
-    }
+}
 
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-
+    
     return self.pivyDic.count;
     
 }
 
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-//    
-//    [[self.pivyDic objectForKey:[self.countrySet ]] count];
-//    
-    return 3;
+    
+    NSString *key = [self.countries objectAtIndex:section];
+    return [[self.pivyDic objectForKey:key] count];
+    
 }
 
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    Pivy *pivy = [[Pivy alloc]init];
+    pivy = [[self.pivyDic objectForKey:[self.countries objectAtIndex:indexPath.section]] objectAtIndex:indexPath.row];
     
     CollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:_reuseIdentifier forIndexPath:indexPath];
-//    cell.textLabel.text = [self.cellData objectAtIndex:indexPath.row];
     cell.backgroundColor = [UIColor brownColor];
-    cell.imageCell.image = [UIImage imageNamed:@"imageTest.png"];
     cell.layer.cornerRadius = cell.layer.visibleRect.size.height /2;
+    
+    if(pivy.image){
+        
+        cell.imageCell.image = [UIImage imageWithData:[pivy.image getData]];
+    }
+    else{
+        cell.imageCell.image = [UIImage imageNamed:@"imageTest.png"];
+    }
+    
+    NSLog(@"SECTION: %ld    ROW: %ld   PIVY: %@", indexPath.section, indexPath.row, pivy.name  );
     
     return cell;
 }
